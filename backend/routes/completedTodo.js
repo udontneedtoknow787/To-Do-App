e6f35b36ae30d20b6c2d388e202407e38aca5a06
udontneedtoknow787 <145577513+udontneedtoknow787@users.noreply.github.com>
2024-08-24 00:16:49 +0530

    Get Completed todo working fine

--- backend/routes/completedTodo.js
+++ backend/routes/completedTodo.js
@@ -3,11 +3,16 @@ const { ToDo } = require('../db');
 const router = express.Router();
 
 router.get('/', async function(req, res){
-    // const completedTodos = await ToDo.find({
-    //     completed: true
-    // });
+    const completedTodos = await ToDo.find({
+        completed: true
+    });
     // console.log(completedTodos);
-    // return res.json(completedTodos.todos);
+    if(completedTodos){
+        return res.json(completedTodos)
+    }
+    else return res.status(403).json({
+        messgae: "Something went wrong!!!"
+    })
 });
 
 module.exports = router
