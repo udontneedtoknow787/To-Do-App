568b556b67adb217569f7de5353b5b6e0ae62522
udontneedtoknow787 <145577513+udontneedtoknow787@users.noreply.github.com>
2024-08-24 22:47:50 +0530

    all todos recieving correctly, basic todocard complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   //  METHOD 1 FOR USEEFFECT
   async function Chagetodo() {
   try {
-      const res = await axios.get("https://sum-server.100xdevs.com/todos");
-      setTodo(res.data.todos);
+      const res = await axios.get("http://localhost:3000/api/v1/todos/all");
+      setTodo(res.data);
     } catch (error) {
       console.log("server not responding!!!")
       alert("Server not Responding!!")
@@ -26,14 +26,36 @@ function App() {
   //     }
   //   )
   // },[])
+  // async function onComplete({todo}) {
+  //   const todotitle =  todo.title;
+  //   const status = todo.completed;
+  //   try{
+  //     const response = await axios.post("http://localhost:3000/api/v1/todos/update",
+  //       {
+  //         tittle: todotitle,
+  //         completed: (status ? false : true)
+  //       }
+  //     );
+  //     if(response.status==200){
+  //       alert("todo updated succesfully");
+  //       Chagetodo();
+  //     }
+  //     else alert("status not good")
+  //   }
+  //   catch(err){
+  //     alert("post request failed. Update not completed")
+  //   }
+  // }
 
   return (<>
-    <h1>To-Do List below: </h1>
+    <h1 className='font-bold text-xl lg:text-3xl'>To-Do List below: </h1>
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 m-4'>
     {
       todos.map(function(t){
         return <Todocard key={t.id} todo={t} />
       })
     }
+    </div>
     <br />
   <button onClick={Chagetodo} className='bg-red-700 hover:bg-red-400 active:bg-green-800 border-8 border-b-blue-900 border-r-blue-900'>Change todo list </button>
   {console.log(todos)}
--- frontend/src/components/todocard.jsx
+++ frontend/src/components/todocard.jsx
@@ -1,6 +1,22 @@
 export function Todocard({todo}){
-    return <div className="bg-black p-2 flex flex-auto">
-        <h1 className="text-white">{todo.title}</h1>
-        <p className="text-cyan-300">{todo.description}</p>
-    </div>
+    if (todo.completed) {
+        return <div className="bg-green-300 rounded-md p-2">
+            {/* {todo.completed ? <span className="text-xl lg:text-2xl cursor-pointer">&#x2611;</span> :
+                <span className="text-xl lg:text-2xl cursor-pointer">&#x2610;</span>} */}
+
+            <span className="text-xl lg:text-2xl cursor-pointer">&#x2611;</span>
+            <h1 className="font-bold text-lg">{todo.title}</h1>
+            <p className="text-slate-800">{todo.description}</p>
+        </div>
+    }
+    else {
+        return <div className="bg-slate-300 rounded-md p-2">
+            {/* {todo.completed ? <span className="text-xl lg:text-2xl cursor-pointer">&#x2611;</span> :
+                <span className="text-xl lg:text-2xl cursor-pointer">&#x2610;</span>} */}
+
+            <span className="text-xl lg:text-2xl cursor-pointer">&#x2610;</span>
+            <h1 className="font-bold text-lg">{todo.title}</h1>
+            <p className="text-slate-800">{todo.description}</p>
+        </div>
+    }
 }
